003db089dbeb66f569b57b0a3944980c862a83d4
Maciek <msitkowski94@gmail.com>
2021-11-21 16:53:46 +0100

    feat: add first feature

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
+  <p>Really cool feature!</p>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
