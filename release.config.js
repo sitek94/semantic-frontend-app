084e1346cc5ea3c48b66d628b717b4ea5a154d28
Maciek <msitkowski94@gmail.com>
2021-11-21 16:50:21 +0100

    chore: add semantic release plugins

--- release.config.js
+++ release.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  branches: ['main', 'next', { name: 'alpha', prerelease: true }],
+  branches: ['main'],
   plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
     '@semantic-release/npm',
     {
       npmPublish: false,
     },
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["package.json"],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
   ],
 }
